64e666117e88b74723d5b32abd770568b34a37a2
bangbang.S <bangbang.song@gmail.com>
2023-07-13 11:33:29 +0800

    Update ExtendedBottomSheetDialogFragment.kt
    add custorm menu af first position

--- app/src/main/java/org/wikipedia/page/ExtendedBottomSheetDialogFragment.kt
+++ app/src/main/java/org/wikipedia/page/ExtendedBottomSheetDialogFragment.kt
@@ -6,4 +6,51 @@ open class ExtendedBottomSheetDialogFragment : BottomSheetDialogFragment() {
     protected fun disableBackgroundDim() {
         requireDialog().window?.setDimAmount(0f)
     }
+
+        class ExtendedBottomSheetDialog(context: Context, @StyleRes theme: Int) :
+        BottomSheetDialog(context, theme) {
+
+        override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+            window?.let {
+                BreadcrumbsContextHelper.dispatchTouchEvent(it, ev)
+            }
+            return super.dispatchTouchEvent(ev)
+        }
+
+        override fun onActionModeStarted(mode: ActionMode?) {
+            Log.d("ExtendedBottomSheetDialogFragment", "onActionModeStarted() called with: mode = $mode")
+
+            super.onActionModeStarted(mode)
+
+            modifyMenu(mode!!)
+        }
+
+        override fun onActionModeFinished(mode: ActionMode?) {
+            Log.d("ExtendedBottomSheetDialogFragment", "onActionModeFinished() called with: mode = $mode")
+            super.onActionModeFinished(mode)
+        }
+
+        private fun modifyMenu(mode: ActionMode) {
+            val menu = mode.menu
+            val menuItemsList = menu.children.filter {
+                val title = it.title.toString()
+                !title.contains("有道翻译")
+            }.toList()
+            val youdao = menu.children.filter {
+                val title = it.title.toString()
+                title.contains("有道翻译")
+            }.toList()
+
+            menu.clear()
+            youdao.forEach {
+                Log.d("actionMode", "menu: + ${it.title}")
+                menu.add(it.groupId, it.itemId, Menu.NONE, it.title).setIntent(it.intent).icon = it.icon
+            }
+            menuItemsList.forEach {
+                Log.d("actionMode", "menu: + ${it.title}")
+                menu.add(it.groupId, it.itemId, Menu.NONE, it.title).setIntent(it.intent).icon = it.icon
+            }
+        }
+
+    }
 }
